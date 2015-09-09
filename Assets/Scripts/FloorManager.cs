6a897f32ad223ebb36ab1329be969e8774f84cf8
Paul <saerdev@gmail.com>
2015-09-09 13:47:48 -0700

    Game Scene - Version control test 2!

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -8,15 +8,18 @@ public class FloorManager : MonoBehaviour {
     public List<Transform> floors = new List<Transform>();
     public float speed;
 
-    private int numberOfFloors = 5;
-
-
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         floorParent.Translate(Vector3.back * speed * Time.deltaTime);
+
+        //for (int i = 0; i < floors.Count; i++) {
+        //    if (floors[i].position.z < -15f)
+        //        floors[i].position.z =  
+        //}
 	}
 }
